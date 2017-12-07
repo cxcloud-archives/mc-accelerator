20ba29aef3713d0d11bf78a3b9080126de822fd1
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2017-12-07 16:17:23 +0200

    Added clarity to angular project;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ClarityModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
