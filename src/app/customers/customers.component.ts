90ec392ffc7dc40f58cc480946e9275134d46910
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2018-02-01 13:48:40 +0200

    Integration with customers API; Display customers info in data grid;

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../core/customer/customer.service';
+import { Customer } from '@cxcloud/ct-types/customers';
 
 @Component({
   selector: 'app-customers',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customers.component.scss']
 })
 export class CustomersComponent implements OnInit {
+  customers: Customer[];
+  columns: Array<any> = [
+    { name: 'Customer ID', field: 'id' },
+    { name: 'First name', field: 'firstName' },
+    { name: 'Last name', field: 'lastName' },
+    { name: 'Company', field: 'company' },
+    { name: 'Email', field: 'email' },
+    { name: 'Created on', field: 'createdAt' },
+    { name: 'Modified on', field: 'lastModifiedAt' }
+  ];
+  placeholder = '------';
 
-  constructor() { }
+  constructor(private customerService: CustomerService) {}
 
   ngOnInit() {
+    this.customerService
+      .getCustomers()
+      .subscribe(result => (this.customers = result));
   }
-
 }
