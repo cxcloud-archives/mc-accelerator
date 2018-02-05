17bff54884b7786239aeb912c9d862838d3bcf6a
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2018-02-05 11:20:41 +0200

    Add customer details;

--- src/app/customers/customer/customer.component.ts
+++ src/app/customers/customer/customer.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CustomerService } from '../../core';
+import { Customer } from '@cxcloud/ct-types/customers';
+import { Address } from '@cxcloud/ct-types/common';
+
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
-  constructor() {}
+  customer: Customer;
+  noInfo: 'No information';
 
-  ngOnInit() {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private customerService: CustomerService
+  ) {}
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      // Get order by its id
+      this.customerService.getCustomer(params['id']).subscribe(customer => {
+        if (customer) {
+          this.customer = customer;
+          console.log(customer);
+        }
+      });
+    });
+  }
 }
